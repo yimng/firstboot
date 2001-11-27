2a85c5f6760340f9af71ec7c3b340459ad83626e
bfox <bfox>
2001-11-27 02:54:13 +0000

    minor updates to allow pull in for firstboot to work

--- src/modules/firewall.py
+++ src/modules/firewall.py
@@ -4,20 +4,23 @@ from gtk import *
 import string
 import libglade
 import gtk
+import gnome.ui
 
-xml = libglade.GladeXML ("../glade/firewall.glade", domain="redhat-config-firewall")
+xml = libglade.GladeXML ("/home/bfox/redhat/firstboot/glade/firewall.glade", domain="redhat-config-firewall")
 #xml = libglade.GladeXML ("/usr/share/firstboot/glade/firewall.glade", domain="redhat-config-firewall")
 
 class childWindow:
     #You must specify a runPriority for the order in which you wish your module to run
     runPriority = 40
     moduleName = "Firewall"
-
+    moduleClass = "reconfig"
+    
     def __init__(self):
-        self.toplevel = xml.get_widget("mainWindow")
+        self.mainWindow = xml.get_widget("mainWindow")
+        self.toplevel = xml.get_widget("topVBox")
         self.vbox = xml.get_widget("mainVBox")
-        self.deviceList = xml.get_widget("deviceList")
-        self.netdevices = []
+#        self.deviceList = xml.get_widget("deviceList")
+#        self.netdevices = []
                 
     def launch(self):
         #Hack to remove vbox from the toplevel window so it can
@@ -27,32 +30,15 @@ class childWindow:
         else:
             pass
 
-        self.devices = self.availableDevices()
-        for device in self.devices:
-            self.deviceList.append([device])
+#        self.devices = self.availableDevices()
+#        for device in self.devices:
+#            self.deviceList.append([device])
 
         return self.vbox
-
-#        label = GtkLabel("foo")
-#        return label
-
-    def availableDevices(self):
-        f = open("/proc/net/dev")
-        lines = f.readlines()
-        f.close()
-        # skip first two lines, they are header
-        lines = lines[2:]
-        for line in lines:
-            dev = string.strip(line[0:6])
-            if dev != "lo":
-                self.netdevices.append(dev)
-        return self.netdevices
 
     def write_file(self):
         pass
 
     def stand_alone(self):
-#        box = childWindow().launch()
-#        toplevel.add(box)
-        self.toplevel.show_all()
+        self.mainWindow.show_all()
         gtk.mainloop()
