f04139816ad4d72274ca1f93ec888a1cdab78728
bfox <bfox>
2001-12-11 01:58:31 +0000

    port to gtk2

--- src/firstbootWindow.py
+++ src/firstbootWindow.py
@@ -15,10 +15,13 @@ os.environ["PYGTK_FATAL_EXCEPTIONS"] = "1"
 os.environ["GNOME_DISABLE_CRASH_DIALOG"] = "1"
 
 import sys
-from gtk import *
-from gtk import _root_window
-import GDK
-import gdkpixbuf
+#from gtk import *
+#from gtk import _root_window
+#import GDK
+#import gdkpixbuf
+
+import gtk
+import gobject
 
 doReconfig = 1
 
@@ -33,43 +36,40 @@ print doReconfig
 
 class firstbootWindow:
     def __init__(self):
-        self.hpane = GtkHPaned()
+        self.hpane = gtk.HPaned()
         self.moduleList = []
         self.moduleDict = {}
         self.usedModuleList = []
 
-        root = _root_window ()
-        cursor = cursor_new (GDK.LEFT_PTR)
-        root.set_cursor (cursor)
+#        root = _root_window ()
+#        cursor = cursor_new (GDK.LEFT_PTR)
+#        root.set_cursor (cursor)
 
-        width = screen_width()
+#        width = screen_width()
         p = None
         
-        win = GtkWindow()
+        win = gtk.Window()
         win.set_usize(800, 600)
-        win.set_policy(FALSE, FALSE, FALSE)
-        mainVBox = GtkVBox()
+        win.set_policy(gtk.FALSE, gtk.FALSE, gtk.FALSE)
+        mainVBox = gtk.VBox()
 
         try:
-            p = gdkpixbuf.new_from_file("images/titlebar.png")
+            p = gtk.gdk.pixbuf.new_from_file("images/titlebar.png")
         except:
             pass
 
         if p:
-            pix = apply (GtkPixmap, p.render_pixmap_and_mask())
-            mainVBox.pack_start(pix, FALSE, TRUE, 0)
+#            pix = apply (GtkPixmap, p.render_pixmap_and_mask())
+            pix = gtk.Image()
+            pix.set_from_pixbuf(p)
+            mainVBox.pack_start(pix, gtk.FALSE, gtk.TRUE, 0)
         
-        self.stepList = GtkCList()
-        self.stepList.connect("select-row", self.selectRow)
-        self.hpane.set_position(200)
-        self.hpane.add1(self.stepList)
-
-        self.notebook = GtkNotebook()
-        self.notebook.set_show_tabs(FALSE)
-        self.notebook.set_show_border(FALSE)
+        self.notebook = gtk.Notebook()
+        self.notebook.set_show_tabs(gtk.FALSE)
+        self.notebook.set_show_border(gtk.FALSE)
 
 #        path = ('/usr/share/firstboot/modules')
-        path = ('/home/devel/bfox/redhat/firstboot/src/modules')
+        path = ('/home/devel/bfox/redhat/firstboot-gtk2/src/modules')
         files = os.listdir(path)
         list = []
 
@@ -84,7 +84,7 @@ class firstbootWindow:
 #            sys.path.append('./modules')
             print module
 #            sys.path.append('/usr/share/firstboot/modules')
-            sys.path.append('/home/devel/bfox/redhat/firstboot/src/modules')
+            sys.path.append('/home/devel/bfox/redhat/firstboot-gtk2/src/modules')
             cmd = ("import %s\nif %s.__dict__.has_key('childWindow'):"
                    "obj = %s.childWindow()") % (module, module, module)
             exec(cmd)
@@ -113,33 +113,40 @@ class firstbootWindow:
         for module in self.moduleList:
             box = module.launch()
             if box:
-                self.notebook.append_page(box, GtkLabel(" "))
+                self.notebook.append_page(box, gtk.Label(" "))
 
 #        self.hpane.add2(self.notebook)
 
-        bb = GtkHButtonBox()
-        bb.set_layout(BUTTONBOX_END)
-        backButton = GtkButton("Back")
+        bb = gtk.HButtonBox()
+        bb.set_layout(gtk.BUTTONBOX_END)
+        backButton = gtk.Button("Back")
         backButton.connect('clicked', self.backClicked)
-        nextButton = GtkButton("Next")
+        nextButton = gtk.Button("Next")
         nextButton.connect('clicked', self.okClicked)
-        group = GtkAccelGroup()
-        nextButton.add_accelerator('clicked', group, GDK.F12,
-                                   GDK.RELEASE_MASK, 0)
-        backButton.add_accelerator('clicked', group, GDK.F11,
-                                   GDK.RELEASE_MASK, 0)
+        group = gtk.AccelGroup()
+#        nextButton.add_accelerator('clicked', group, GDK.F12,
+#                                   GDK.RELEASE_MASK, 0)
+#        backButton.add_accelerator('clicked', group, GDK.F11,
+#                                   GDK.RELEASE_MASK, 0)
         win.add_accel_group(group)
         bb.pack_start(backButton)
         bb.pack_start(nextButton)
 
 #        mainVBox.pack_start(self.hpane)
         mainVBox.pack_start(self.notebook)
-        mainVBox.pack_start(bb, FALSE, 20)
+        self.notebook.set_border_width(10)
+
+#        a = GtkAlignment()
+#        a.add(GtkHSeparator())
+#        a.set(0.0, 0.0, 0.5, 0.5)
+        mainVBox.pack_start(gtk.HSeparator(), gtk.FALSE)
+
+        mainVBox.pack_start(bb, gtk.FALSE, padding=10)
         win.add(mainVBox)
         win.show_all()
-        self.notebook.set_page(0)
-        self.stepList.select_row(0, 0)
-        mainloop()
+#        self.notebook.set_page(0)
+#        self.stepList.select_row(0, 0)
+        gtk.main()
 
     def okClicked(self, *args):
         module = self.moduleList[self.notebook.get_current_page()]
@@ -149,11 +156,11 @@ class firstbootWindow:
         except:
             pass
         self.notebook.next_page()
-        self.stepList.select_row(self.notebook.get_current_page(), 0)
+#        self.stepList.select_row(self.notebook.get_current_page(), 0)
 
     def backClicked(self, *args):
         self.notebook.prev_page()
-        self.stepList.select_row(self.notebook.get_current_page(), 0)
+#        self.stepList.select_row(self.notebook.get_current_page(), 0)
 
     def selectRow(self, list, row, col, event):
         self.notebook.set_page(row)
