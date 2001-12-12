79caf3b07d9281ca05ebb0b9923d80b346c0e146
bfox <bfox>
2001-12-12 07:34:50 +0000

    merge in messages

--- src/modules/register.py
+++ src/modules/register.py
@@ -1,27 +1,60 @@
-#!/usr/bin/python
-
 from gtk import *
 import string
+import gtk
+import gobject
 import os
 from socket import gethostname
 from socket import gethostbyname
 
 class childWindow:
     #You must specify a runPriority for the order in which you wish your module to run
-    runPriority = 120
+    runPriority = 110
     moduleName = "Register with RHN"
 
     def __init__(self):
-        print "initializing RHN module"
-
+        print "initializing register module"
+                
     def launch(self):
-        self.vbox = GtkVBox()
+        self.vbox = gtk.VBox()
+        self.vbox.set_usize(400, 200)
+
+        label = gtk.Label("Register your system with Red Hat Network")
+        label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse ("white"))
+
+        titleBox = gtk.HBox()
+
+        try:
+            p = gtk.gdk.pixbuf_new_from_file("images/networking.png")
+        except:
+            pass
+
+        if p:
+            pix = gtk.Image()
+            pix.set_from_pixbuf(p)
+            titleBox.pack_start(pix, gtk.FALSE, gtk.TRUE, 5)
+
+
+        
+        titleBox.pack_start(label)
+
+        eventBox = gtk.EventBox()
+        eventBox.add(titleBox)
+        eventBox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse ("#cc0000"))
+        self.vbox.pack_start(eventBox, FALSE)
+
+#        self.vbox.pack_start(label, FALSE, TRUE, 30)
+        a = gtk.Alignment()
+        a.add(gtk.HSeparator())
+        a.set(0.5, 0.5, 1.0, 1.0)
+        
+        self.vbox.pack_start(a, FALSE)
+
+        internalVBox = gtk.VBox()
+        internalVBox.set_border_width(10)
+
 
         if self.networkAvailable() == TRUE:
-#            label = GtkLabel("Register your system with Red Hat Network")
-
-
-            label = GtkLabel("Red Hat Network is an Internet solution for managing "
+            label = gtk.Label("Red Hat Network is an Internet solution for managing "
                              "one or more Red Hat Linux systems. All Security Alerts, "
                              "Bug Fix Alerts, and Enhancement Alerts (collectively known as "
                              "Errata Alerts) can be retreived directly from Red Hat. You can "
@@ -30,39 +63,51 @@ class childWindow:
                              "Because Red Hat Network keeps track of when Errata Updates are "
                              "released and sends you email notifications, it can:")
             label.set_line_wrap(TRUE)
-            label.set_usize(600, -1)
+            label.set_usize(400, -1)
             label.set_alignment(0.0, 0.5)
-            self.vbox.pack_start(label, FALSE, TRUE)
+            internalVBox.pack_start(label, FALSE, TRUE)
 
-            label = GtkLabel("* Reduce the time and effort required by system administrators "
+            label = gtk.Label(" -- Reduce the time and effort required by system administrators "
                              "to stay on top of the Red Hat errata list\n"
-                             "* Minimize security vulnerabilities in your network by providing "
+                             " -- Minimize security vulnerabilities in your network by providing "
                              "the patches as soon as Red Hat releases them\n"
-                             "* Filter out package updates not relevant to your network\n"
-                             "* Schedule Errata Updates so that packages are delivered to "
+                             " -- Filter out package updates not relevant to your network\n"
+                             " -- Schedule Errata Updates so that packages are delivered to "
                              "selected systems when you want it")
             label.set_line_wrap(TRUE)
             label.set_alignment(0.1, 0.5)
             label.set_usize(600, -1)
 #To start using Red Hat Network today, follow these steps:
 
-            self.vbox.pack_start(label, FALSE, TRUE)
-            launchButton = GtkButton("Register system now")
+            internalVBox.pack_start(label, FALSE, TRUE)
+            launchButton = gtk.Button("Register system now")
 
-            a = GtkAlignment()
+            a = gtk.Alignment()
             a.add(launchButton)
             a.set(0.3, 0.0, 0.3, 0.5)
-            self.vbox.pack_start(a, FALSE, padding=10)
+            internalVBox.pack_start(a, FALSE, padding=10)
 
             launchButton.connect("clicked", self.rhn_register)
-#            self.vbox.pack_start(launchButton, FALSE, FALSE)
+#            internalVBox.pack_start(launchButton, FALSE, FALSE)
         else:
-            label = GtkLabel("You currently have no network.")
-            self.vbox.pack_start(label, FALSE, TRUE, 30)
+            label = gtk.Label("You currently have no network.")
+            internalVBox.pack_start(label, FALSE, TRUE, 30)
 #            launchButton = GtkButton("Register system now")
 #            launchButton.connect("clicked", self.rhn_register)
-#            self.vbox.pack_start(launchButton, FALSE)
+#            internalVBox.pack_start(launchButton, FALSE)
 
+
+
+
+
+#        label = gtk.Label("Welcome message goes here")
+
+
+
+        eventBox = gtk.EventBox()
+        eventBox.add(internalVBox)
+        eventBox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#EEEEEE"))
+        self.vbox.pack_start(eventBox, TRUE)
 
         return self.vbox
 
@@ -83,8 +128,10 @@ class childWindow:
         if (not win):
             print "launching rhn_register"
             path = "/usr/sbin/rhn_register"
-            os.execv(path, [])
+            os.execv(path, [""])
 
     def write_file(self):
         pass
 
+    def apply(self):
+        pass
