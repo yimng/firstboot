b1febfdd7c74146e57949ec118d5a0167224528f
bfox <bfox>
2002-05-20 16:08:26 +0000

    start on adding some version checking

--- src/firstboot.py
+++ src/firstboot.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python2.2
 
-import sys, os
+import sys
+import os
+import string
 import signal
 import time
 
 x_class = None
 wm_pid = None
+doDebug = None
+FILENAME = "/etc/sysconfig/firstboot"
+
+for arg in sys.argv:
+    if arg == '--reconfig':
+        print "starting reconfig mode"
+        doReconfig = 1
+    if arg == '--debug':
+        print "starting with debugging options"
+        doDebug = 1
 
 if __name__ == "__main__":
     signal.signal (signal.SIGINT, signal.SIG_DFL)
@@ -44,6 +56,32 @@ def setRootBackground():
         args = [path, '-solid', 'midnightblue']
         os.execv(path, args)
 
+
+#Let's check to see if firstboot should be run or not
+#If we're in debug mode, run anyway.  Even if the file exists
+if (not doDebug):
+    #We're not in debug mode, so do some checking
+    #First, look and see if /etc/sysconfig/firstboot exists
+    try:
+        os.stat (FILENAME)
+
+        fd = open(FILENAME, 'r')
+        lines = fd.readlines()
+        fd.close()
+
+        for line in lines:            
+            print "line is", line
+            line = string.strip(line)
+            if (string.find(line, "VERSION") > -1) and line[0] != "#":
+                if string.find(line, "="):
+                    key, value = string.split(line, "=")
+                    print key, value
+
+        print "file exists"
+        os._exit(0)
+    except:
+        #Firstboot has never been run before, so start it up
+        print "file doesn't exist"
 
 
 if not os.environ.has_key('DISPLAY'):
