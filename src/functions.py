df99c957fe02183b888e2d8629cfe1fff8603947
bfox <bfox>
2002-05-30 21:17:55 +0000

    we will collect common functions here

---
+++ src/functions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2.2
+
+import gtk
+
+# Attempt to load a gtk.Image from a file.
+def imageFromFile(filename):
+    p = None        
+    try:
+        path = "pixmaps/" + filename
+        p = gtk.gdk.pixbuf_new_from_file(path)
+    except:
+        try:
+            path = "/usr/share/firstboot/pixmaps/" + filename
+            p = gtk.gdk.pixbuf_new_from_file(path)
+        except:
+            pass
+
+    if p:
+        pix = gtk.Image()
+        pix.set_from_pixbuf(p)        
+        return pix
+
