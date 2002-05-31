9766371332396dba91e09d164714e51f24e19b6d
bfox <bfox>
2002-05-31 23:27:22 +0000

    make table autofill

--- src/modules/hardware.py
+++ src/modules/hardware.py
@@ -6,7 +6,9 @@ import os
 import sys
 import functions
 sys.path.append('/usr/share/redhat-config-soundcard')
+sys.path.append('/usr/share/redhat-config-keyboard')
 import soundcardBackend
+import rhpl.keyboard as keyboard
 
 ##
 ## I18N
@@ -37,7 +39,6 @@ class childWindow:
         titleBox = gtk.HBox()
 
         pix = functions.imageFromFile("hwbrowser.png")
-        print pix
         titleBox.pack_start(pix, gtk.FALSE, gtk.TRUE, 5)
 
         titleBox.pack_start(label)
@@ -47,41 +48,57 @@ class childWindow:
         self.vbox.pack_start(eventBox, FALSE)
 
         self.table = gtk.Table(3, 4)
+        self.table.set_border_width(6)
         self.table.set_col_spacings(6)
         self.table.set_row_spacings(6)
         self.vbox.pack_start(self.table)
 
         self.table_row = 0
 
+        self.configureKeyboard()
+        self.configureMouse()
         self.configureSoundcard()
         self.configureNetworking()
         self.configureDisplay()
         self.configurePrinters()
-        
-#        pix = self.imageFromFile("splash.png")
-#        self.vbox.pack_start(pix, gtk.FALSE, gtk.TRUE, 5)
 
-
-#        label = gtk.Label("Welcome message goes here")
-#        self.vbox.pack_start(label, TRUE)
-
-#        self.hbox = gtk.HBox()
-#        self.hbox.pack_start(self.splash, gtk.FALSE)
-#        self.hbox.pack_start(label, gtk.TRUE)
-#        self.vbox.pack_start(self.hbox, gtk.TRUE)
-
-            
         return self.vbox, eventBox
 
     def apply(self, notebook):
         return 1
+
+    def configureKeyboard(self):
+        kbd = keyboard.Keyboard()
+        kbd.read()
+        layout, keymap, keys, variant = kbd.modelDict[kbd.get()]
+        
+        
+        pix = functions.imageFromFile("keyboard.png")
+        self.table.attach(pix, 0, 1, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
+        label = gtk.Label(_("Keyboard Map:"))
+        label.set_alignment(0.0, 0.5)
+        self.table.attach(label, 1, 2, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
+        self.keyboard_label = gtk.Label(layout)
+        self.keyboard_label.set_alignment(0.0, 0.5)
+        self.table.attach(self.keyboard_label, 2, 3, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
+        self.keyboard_button = gtk.Button(_("Configure..."))
+        self.keyboard_button.connect("clicked", self.keyboard_button_clicked)
+        self.table.attach(self.keyboard_button, 3, 4, self.table_row, self.table_row + 1, gtk.EXPAND, gtk.SHRINK)
+        self.table_row = self.table_row + 1
+
+    def configureMouse(self):
+        pix = functions.imageFromFile("mouse.png")
+        self.table.attach(pix, 0, 1, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
+        label = gtk.Label(_("Mouse:"))
+        label.set_alignment(0.0, 0.5)
+        self.table.attach(label, 1, 2, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
+        self.table_row = self.table_row + 1
 
     def configureSoundcard(self):
         self.soundcardBackend = soundcardBackend.soundcardBackend()
         cards = self.soundcardBackend.probeCards()
         if not cards:
             return
-        print cards
         self.device, self.module, self.description = self.soundcardBackend.getData(cards[0])
 
         pix = functions.imageFromFile("multimedia.png")
@@ -89,7 +106,7 @@ class childWindow:
         label = gtk.Label(_("Sound card:"))
         label.set_alignment(0.0, 0.5)
         self.table.attach(label, 1, 2, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
-        self.soundcard_label = gtk.Label(_("Sound card:"))
+        self.soundcard_label = gtk.Label(_(""))
         self.soundcard_label.set_alignment(0.0, 0.5)
         self.table.attach(self.soundcard_label, 2, 3, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
         self.soundcard_button = gtk.Button(_("Configure..."))
@@ -103,28 +120,43 @@ class childWindow:
 
     def configureNetworking(self):
         pix = functions.imageFromFile("networking.png")
-        self.table.attach(pix, 0, 1, 1, 2, gtk.FILL, gtk.FILL)
+        self.table.attach(pix, 0, 1, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
         label = gtk.Label(_("Networking:"))
         label.set_alignment(0.0, 0.5)
-        self.table.attach(label, 1, 2, 1, 2, gtk.FILL, gtk.FILL)
+        self.table.attach(label, 1, 2, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
+        self.table_row = self.table_row + 1
 
     def configureDisplay(self):
         pix = functions.imageFromFile("display.png")
-        self.table.attach(pix, 0, 1, 2, 3, gtk.FILL, gtk.FILL)
+        self.table.attach(pix, 0, 1, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
         label = gtk.Label(_("Display:"))
         label.set_alignment(0.0, 0.5)
-        self.table.attach(label, 1, 2, 2, 3, gtk.FILL, gtk.FILL)
+        self.table.attach(label, 1, 2, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
+        self.table_row = self.table_row + 1
 
     def configurePrinters(self):
         pix = functions.imageFromFile("printer.png")
-        self.table.attach(pix, 0, 1, 3, 4, gtk.FILL, gtk.FILL)
+        self.table.attach(pix, 0, 1, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
         label = gtk.Label(_("Printers:"))
         label.set_alignment(0.0, 0.5)
-        self.table.attach(label, 1, 2, 3, 4, gtk.FILL, gtk.FILL)
-
+        self.table.attach(label, 1, 2, self.table_row, self.table_row + 1, gtk.FILL, gtk.FILL)
+        self.table_row = self.table_row + 1
 
         
     ############Event Handlers#############
+    def keyboard_button_clicked(self, *args):
+        pass
+#        win = os.fork()
+
+##         if (not win):
+##             if self.doDebug:
+##                 print "launching redhat-config-keyboard"
+##             path = "/usr/share/redhat-config-keyboard/redhat-config-keyboard.py"
+##             os.execv(path, [""])
+#         import keyboard_gui
+#         app = keyboard_gui.childWindow()
+#         app.stand_alone()
+
         
     def soundcard_button_clicked(self, *args):
         win = os.fork()
@@ -134,4 +166,3 @@ class childWindow:
                 print "launching redhat-config-soundcard"
             path = "/usr/share/redhat-config-soundcard/redhat-config-soundcard.py"
             os.execv(path, [""])
-
