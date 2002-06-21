b0f7253b3ceacee15dae0472452aebcd0e2b4a8f
bfox <bfox>
2002-06-21 15:32:09 +0000

    new firstboot build script

---
+++ build_all.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+make snapsrc
+cd ..
+cd redhat-config-keyboard
+make snapsrc
+cd ..
+cd redhat-config-language
+make snapsrc
+cd ..
+cd redhat-config-mouse
+make snapsrc
+cd ..
+cd redhat-config-rootpassword
+make snapsrc
+cd ..
+cd redhat-config-securitylevel
+make snapsrc
+cd ..
+cd redhat-config-soundcard
+make snapsrc
