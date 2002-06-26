33bd475a7144bf7e52902b8c599ebf401fc37e97
bfox <bfox>
2002-06-26 19:25:34 +0000

    add some print statements

--- check_status.sh
+++ check_status.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 
-
+echo "Checking firstboot"
 cvs status 2>/dev/null| grep Status | grep -v Up
 cd ..
 cd redhat-config-keyboard
+echo "Checking redhat-config-keyboard"
 cvs status 2>/dev/null| grep Status | grep -v Up
 cd ..
 cd redhat-config-language
+echo "Checking redhat-config-language"
 cvs status 2>/dev/null| grep Status | grep -v Up
 cd ..
 cd redhat-config-mouse
+echo "Checking redhat-config-mouse"
 cvs status 2>/dev/null| grep Status | grep -v Up
 cd ..
 cd redhat-config-rootpassword
+echo "Checking redhat-config-password"
 cvs status 2>/dev/null| grep Status | grep -v Up
 cd ..
 cd redhat-config-securitylevel
+echo "Checking redhat-config-securitylevel"
 cvs status 2>/dev/null| grep Status | grep -v Up
 cd ..
 cd redhat-config-soundcard
+echo "Checking redhat-config-soundcard"
 cvs status 2>/dev/null| grep Status | grep -v Up
 cd ..
 cd redhat-config-nfs
+echo "Checking redhat-config-nfs"
 cvs status 2>/dev/null| grep Status | grep -v Up
 cd ..
 cd redhat-config-samba
+echo "Checking redhat-config-samba"
 cvs status 2>/dev/null| grep Status | grep -v Up
