1cebfd9a8a6ebc5a5f9702f953cffe11355d3072
bfox <bfox>
2002-07-02 21:39:23 +0000

    major UI improvements

--- src/firstbootWindow.py
+++ src/firstbootWindow.py
@@ -27,13 +27,13 @@ class firstbootWindow:
         self.mainHBox = gtk.HBox(gtk.FALSE, 10)
         self.moduleList = []
         self.moduleDict = {}
-        self.moduleStore = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_INT)
 
         # Create the initial window and a vbox to fill it with.
         win = gtk.Window()
         win.connect("destroy", self.destroy)
 
         win.connect ("key-release-event", self.keyRelease)
+        print "Screen width is", gtk.gdk.screen_width()
 
         if gtk.gdk.screen_width() >= 800:            
             win.set_size_request(800, 600)
@@ -52,7 +52,9 @@ class firstbootWindow:
         if self.doDebug:
             print "starting firstbootWindow", doReconfig, doDebug                    
             path = ('modules/')
+            self.notebook.set_show_tabs(gtk.FALSE)
             self.notebook.set_scrollable(gtk.TRUE)
+            self.notebook.set_show_border(gtk.FALSE)
         else:
             path = ('/usr/share/firstboot/modules')
             self.notebook.set_show_tabs(gtk.FALSE)
@@ -61,10 +63,10 @@ class firstbootWindow:
         sys.path.append(path)
 
         #Code for an upper title bar like anaconda.  We may turn this on if we get some UI help
-##         pix = self.imageFromFile("pixmaps/titlebar.png")
-##         if pix:
-##             mainVBox.pack_start(pix, gtk.FALSE, gtk.TRUE, 0)
-##             pass
+        pix = self.imageFromFile("firstboot-header.png")
+        if pix:
+            mainVBox.pack_start(pix, gtk.FALSE, gtk.TRUE, 0)
+            pass
 
         # Generate a list of all of the module files (which becomes the list of
         # all non-hidden files in the directory with extensions other than .py.
@@ -99,6 +101,11 @@ class firstbootWindow:
         modulePriorityList = self.moduleDict.keys()
         modulePriorityList.sort()
 
+        leftVBox = gtk.VBox()
+        leftLabelVBox = gtk.VBox()
+        leftVBox.pack_start(leftLabelVBox, gtk.TRUE)
+        leftVBox.set_border_width(12)
+
 	# Add the modules to the proper lists.
         pages = 0
         for priority in modulePriorityList:
@@ -122,43 +129,31 @@ class firstbootWindow:
                 self.moduleList.append(module)
                 # Set the background of the header to a uniform color.
                 eventbox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#6d81a0"))
-                # If the module has a name, add it to the ListStore as well, mapping
-                # the module name to the page number.
+                # If the module has a name, add it to the list of labels
                 if hasattr(module, "moduleName"):
                     self.notebook.append_page(vbox, gtk.Label(module.moduleName))
-                    iter = self.moduleStore.append()
-                    self.moduleStore.set_value(iter, 0, module.moduleName)
-                    self.moduleStore.set_value(iter, 1, pages)
+                    label = gtk.Label(module.moduleName)
+                    label.set_alignment(0.0, 0.5)
+                    leftLabelVBox.pack_start(label, gtk.FALSE, 5)
                 else:
                     self.notebook.append_page(vbox, gtk.Label(" "))
                 pages = pages + 1
 
-        # Create the TreeView widget and pack it into a box.
-        self.moduleView = gtk.TreeView(self.moduleStore)
-        self.moduleView.connect("cursor-changed", self.cursorChanged)
-        leftVBox = gtk.VBox()
-        leftVBox.pack_start(self.moduleView, gtk.TRUE)
-
         # Hook up a signal to highlight the selected page, and switch to page 0.
-        self.notebook.connect("switch-page", self.switchPage)
+#        self.notebook.connect("switch-page", self.switchPage)
         self.notebook.set_current_page(0)
 
         # Add our logo to the left box, below the TreeView.
-        pix = self.imageFromFile("redhat-logo.png")
-        if pix:
-            leftVBox.pack_start(pix, gtk.FALSE)
-
-        # Tell the TreeView how to display the ListStore.
-        col = gtk.TreeViewColumn(None, gtk.CellRendererText(), text=0)
-        self.moduleView.append_column(col)
-        self.moduleView.set_property("headers-visible", gtk.FALSE)
-        self.moduleView.set_size_request(195, -1)
+#        pix = self.imageFromFile("redhat-logo.png")
+#        if pix:
+#            leftVBox.pack_start(pix, gtk.FALSE)
 
         # Add the box on the left to the window's HBox.
         self.mainHBox.pack_start(leftVBox, gtk.FALSE)
 
         # Populate the right side of the window.  Add the notebook to a VBox.
         self.internalVBox = gtk.VBox()
+
         self.internalVBox.pack_start(self.notebook, gtk.TRUE)
 
         # Now add the VBox to an EventBox.
@@ -166,16 +161,21 @@ class firstbootWindow:
         eventBox.add(self.internalVBox)
         eventBox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#EEEEEE"))
 
+        borderBox = gtk.VBox()
+        borderFrame = gtk.Frame()
+        borderBox.pack_start(borderFrame)
+
         # Now add the EventBox to the right-side VBox.
         self.rightVBox = gtk.VBox()
         self.rightVBox.set_size_request(400, 200)
-        self.rightVBox.pack_start(eventBox, gtk.TRUE)
+        self.rightVBox.pack_start(eventBox)
+        borderFrame.add(self.rightVBox)
 
-        # Add an Alignment widget to the top-level HBox, and place the
-	# right-side VBox into it.
+        # Add an Alignment widget to the top-level HBox, and place the right-side VBox into it.
         alignment = gtk.Alignment()
-        alignment.add(self.rightVBox)
-        alignment.set(0.5, 0.5, 0.9, 0.9)
+#        alignment.add(self.rightVBox)
+        alignment.add(borderBox)
+        alignment.set(0.2, 0.2, 0.8, 0.9)
         self.mainHBox.pack_start(alignment, gtk.TRUE)
 
         # Create a lower hbox that will contain the close button and button box
@@ -204,28 +204,13 @@ class firstbootWindow:
         # Create the "go forward" and "finish" buttons.
         self.nextButton = gtk.Button(stock='gtk-go-forward')
         self.nextButton.connect('clicked', self.nextClicked)
-#        group = gtk.AccelGroup()
-#        self.nextButton.add_accelerator("clicked", group, gtk.gdk.keyval_from_name("p"), gtk.gdk.RELEASE_MASK, 0)
-#        win.add_accel_group(group)
 
         self.bb.pack_start(self.nextButton)
         self.finishButton = gtk.Button(stock='gtk-close')
         self.finishButton.connect('clicked', self.finishClicked)
-	# Add the button box to the bottom of the box which contains the
-	# notebook.
-        self.lowerHBox.pack_start(self.bb, gtk.TRUE)
-        
-#        self.internalVBox.pack_start(self.bb, gtk.FALSE, 10)
+	# Add the button box to the bottom of the box which contains the notebook.
+        self.lowerHBox.pack_start(self.bb, gtk.TRUE)        
         self.internalVBox.pack_start(self.lowerHBox, gtk.FALSE, 10)
-
-
-        #Accelerators aren't currently working in GTK 2.0   Grrrrrrr.
-        group = gtk.AccelGroup()
-        self.nextButton.add_accelerator('clicked', group, gtk.keysyms.F12,
-                                   gtk.gdk.RELEASE_MASK, 0)
-        self.backButton.add_accelerator('clicked', group, gtk.keysyms.F11,
-                                   gtk.gdk.RELEASE_MASK, 0)
-        win.add_accel_group(group)
 
 	# Add the main HBox to a VBox which will sit in the window.
         mainVBox.pack_start(self.mainHBox)
@@ -274,13 +259,9 @@ class firstbootWindow:
             fd.write("RUN_FIRSTBOOT=NO\n")
             fd.close()
 
-#            pid = os.fork()
-#
-#            #Remove the firstboot init script
-#            if (not pid):
-#                path = '/sbin/chkconfig'
-#                args = ['--delete', 'firstboot']
-#                os.execv(path, args)
+            #Turn off the firstboot init script
+            path = "/sbin/chkconfig --level 35 firstboot off"
+            os.system(path)
 
         #Exit the GTK loop
         gtk.mainquit()
@@ -289,8 +270,6 @@ class firstbootWindow:
             os.kill(self.wm_pid, 15)
         #Exit firstboot.  This should take down the X server as well
         os._exit(0)
-
-
 
     def nextClicked(self, *args):
         try:
@@ -336,36 +315,11 @@ class firstbootWindow:
             self.bb.pack_end(self.nextButton)
             self.bb.show_all()
 
-#        self.stepList.select_row(self.notebook.get_current_page(), 0)
-
     def keyRelease(self, window, event):
         if (event.keyval == gtk.keysyms.F12):
             self.nextClicked()
         if (event.keyval == gtk.keysyms.F11):
             self.backClicked()
-
-    # When a row in the list is selected, select the corresponding page in
-    # the notebook.
-    def cursorChanged(self, list):
-        selection = self.moduleView.get_selection()
-        model, iter = selection.get_selected()
-        if model and iter:
-            row = model.get_value(iter, 1)
-            self.notebook.set_current_page(row)
-
-    # When the notebook page is changed, select the right item in the list.
-    def switchPage(self, notebook, page_widget, page):
-        iter = self.moduleStore.get_iter_root()
-        selection = self.moduleView.get_selection()
-        pnum = self.moduleStore.get_value(iter, 1)
-        if (pnum == page):
-            selection.select_iter(iter)
-            return
-	while self.moduleStore.iter_next(iter):
-            pnum = self.moduleStore.get_value(iter, 1)
-            if (pnum == page):
-                selection.select_iter(iter)
-                break;
 
     # Attempt to load a gtk.Image from a file.
     def imageFromFile(self, filename):
