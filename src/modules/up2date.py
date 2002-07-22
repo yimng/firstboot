de942ba6cbb79e23ad25523ea0fc8884af1e5236
bfox <bfox>
2002-07-22 23:24:43 +0000

    remove try/except check

--- src/modules/up2date.py
+++ src/modules/up2date.py
@@ -22,65 +22,61 @@ class childWindow:
         if doDebug:
             print "launching up2date module"
 
-        try:
-            os.stat('/etc/sysconfig/rhn/rhn_register')
-            self.vbox = gtk.VBox()
-            self.vbox.set_size_request(400, 200)
+        self.vbox = gtk.VBox()
+        self.vbox.set_size_request(400, 200)
 
-            label = gtk.Label(_("Red Hat Update Agent"))
-            label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse ("white"))
+        label = gtk.Label(_("Red Hat Update Agent"))
+        label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse ("white"))
 
-            titleBox = gtk.HBox()
+        titleBox = gtk.HBox()
 
-            pix = functions.imageFromFile("up2date.png")
-            if pix:
-                titleBox.pack_start(pix, gtk.FALSE, gtk.TRUE, 5)
-            
-            titleBox.pack_start(label)
+        pix = functions.imageFromFile("up2date.png")
+        if pix:
+            titleBox.pack_start(pix, gtk.FALSE, gtk.TRUE, 5)
 
-            eventBox = gtk.EventBox()
-            eventBox.add(titleBox)
-            eventBox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse ("#cc0000"))
-            self.vbox.pack_start(eventBox, FALSE)
+        titleBox.pack_start(label)
 
-            a = gtk.Alignment()
-            a.add(gtk.HSeparator())
-            a.set(0.5, 0.5, 1.0, 1.0)
+        eventBox = gtk.EventBox()
+        eventBox.add(titleBox)
+        eventBox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse ("#cc0000"))
+        self.vbox.pack_start(eventBox, FALSE)
 
-            self.vbox.pack_start(a, FALSE)
+        a = gtk.Alignment()
+        a.add(gtk.HSeparator())
+        a.set(0.5, 0.5, 1.0, 1.0)
 
-            internalVBox = gtk.VBox()
-            internalVBox.set_border_width(10)
+        self.vbox.pack_start(a, FALSE)
 
-            label = gtk.Label(_("Now that you are registered with Red Hat Network, you can run "
-                             "the Red Hat Update Agent to receive the latest software "
-                             "packages directly from Red Hat.  Using this tool will allow you "
-                             "to always have the most up-to-date Red Hat Linux system "
-                             "with all the security patches, bug fixes, and software "
-                             "package enhancements."))
-            label.set_line_wrap(TRUE)
-            label.set_size_request(400, -1)
-            label.set_alignment(0.0, 0.5)
-            internalVBox.pack_start(label, FALSE, TRUE)
+        internalVBox = gtk.VBox()
+        internalVBox.set_border_width(10)
 
-            radioBox = gtk.VBox()
+        label = gtk.Label(_("Now that you are registered with Red Hat Network, you can run "
+                         "the Red Hat Update Agent to receive the latest software "
+                         "packages directly from Red Hat.  Using this tool will allow you "
+                         "to always have the most up-to-date Red Hat Linux system "
+                         "with all the security patches, bug fixes, and software "
+                         "package enhancements."))
+        label.set_line_wrap(TRUE)
+        label.set_size_request(400, -1)
+        label.set_alignment(0.0, 0.5)
+        internalVBox.pack_start(label, FALSE, TRUE)
 
-            self.radioYes = gtk.RadioButton(None, _("Yes, I would like to register with Red Hat Network"))
-            radioNo = gtk.RadioButton(self.radioYes, _("No, I do not want to register my system."))
+        radioBox = gtk.VBox()
 
-            radioBox.pack_start(self.radioYes, gtk.FALSE)
-            radioBox.pack_start(radioNo, gtk.FALSE)
+        self.radioYes = gtk.RadioButton(None, _("Yes, I would like to register with Red Hat Network"))
+        radioNo = gtk.RadioButton(self.radioYes, _("No, I do not want to register my system."))
 
-            a = gtk.Alignment()
-            a.add(radioBox)
-            a.set(0.3, 0.0, 0.3, 0.5)
-            internalVBox.pack_start(a, FALSE, padding=10)
+        radioBox.pack_start(self.radioYes, gtk.FALSE)
+        radioBox.pack_start(radioNo, gtk.FALSE)
 
-            self.vbox.pack_start(internalVBox, TRUE)
+        a = gtk.Alignment()
+        a.add(radioBox)
+        a.set(0.3, 0.0, 0.3, 0.5)
+        internalVBox.pack_start(a, FALSE, padding=10)
 
-            return self.vbox, eventBox
-        except:
-            return
+        self.vbox.pack_start(internalVBox, TRUE)
+
+        return self.vbox, eventBox
 
     def run_up2date(self, *args):
         #Run rhn_register so they can register with RHN
