0c964217165776ab0a3ef2ba8337d96cd483e77f
bfox <bfox>
2002-08-01 19:23:51 +0000

    improve contents of traceback message

--- src/exceptionWindow.py
+++ src/exceptionWindow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.2
 
 import gtk
+import os
 
 ##
 ## I18N
@@ -11,11 +12,11 @@ gettext.textdomain ("firstboot")
 _=gettext.gettext
 
 class ExceptionWindow:
-    def __init__ (self, module, text):
+    def __init__ (self, module, traceback):
         win = gtk.Dialog()
         win.set_size_request(400, 300)
 
-        self.okButton = win.add_button('gtk-quit', 0)
+        self.okButton = win.add_button('gtk-ok', 0)
         self.okButton.connect("clicked", self.destroy)
 
         text_scroll = gtk.ScrolledWindow()
@@ -27,13 +28,29 @@ class ExceptionWindow:
         text_buf = gtk.TextBuffer(None)
         text_scroll.add(text_view)
 
-        label = gtk.Label(_("An error has occured in module %s." % module.moduleName))
+        label = gtk.Label(_("An error has occured in the %s module." % module.moduleName))
         label.set_alignment(0.0, 0.5)
+
+        explanation = (_("Since there is a problem with the %s module, \n"
+                     "Red Hat Setup Agent will not load this module and \n"
+                     "will attempt to run the remaining modules." % module.moduleName))
+
+        bugzilla = (_("Please file a bug against 'firstboot' in the Red Hat \n"
+                      "bug tracking system at http://www.redhat.com/bugzilla. \n"
+                      "A copy of the debug output has been saved to /tmp/firstboot.txt \n"
+                      "Be sure to attach that file to the bug report. \n"))
+
+        text = traceback + "\n\n" + explanation + "\n\n" + bugzilla
+
         text_buf.set_text(text)
         text_view.set_buffer(text_buf)
 
         win.vbox.pack_start(label, gtk.FALSE)
         win.vbox.pack_start(text_scroll, gtk.TRUE)
+
+        fd = open('/tmp/firstboot.txt', "w")
+        fd.write(traceback)
+        fd.close()
         
         win.show_all()
         win.run()
