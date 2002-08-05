9b6079c05413e2c5049ccc30cec2ab21b0b176b7
bfox <bfox>
2002-08-05 20:44:25 +0000

    convert soundcard_gui.py away from firstboot_gui_window.py

--- src/modules/soundcard_gui.py
+++ src/modules/soundcard_gui.py
@@ -43,55 +43,77 @@ gettext.bindtextdomain ("firstboot", "/usr/share/locale")
 gettext.textdomain ("firstboot")
 _=gettext.gettext
 
-class SoundWindow(FirstbootGuiWindow):
+class SoundWindow:
     #You must specify a runPriority for the order in which you wish your module to run
     runPriority = 110
     moduleName = _("Sound Card")
     windowTitle = _("Sound Card Configuration")
     shortMessage = _("A sound card has been detected on your computer.")
     
-    def getNext(self):
-        pass
-    
-    def destroy(self, args):
-        gtk.mainquit()
-    
-    def setupScreen(self):
+    def __init__(self):
         #Initialize soundcard page
         self.soundcardPage = soundcard.childWindow()
         self.notebook = self.soundcardPage.getNotebook()
         self.notebook.set_border_width(5)
 
-        #Add icon to the top frame
-        self.icon = functions.imageFromFile("multimedia.png")
-#        self.icon = functions.imageFromPath("/usr/share/redhat-config/soundcard/pixmaps/multimedia.png")
+        self.soundcardBackend = soundcardBackend.soundcardBackend()
+        
+    def launch(self, doDebug = None):
+        cards = self.soundcardBackend.probeCards()
 
-        label = gtk.Label(_("Click the "
-                         "\"Play test sound\" button to hear a sample sound.  You should "
-                         "hear a series of three sounds.  The first sound will be in the "
-                         "right channel, the second sound will be in the left channel, "
-                         "and the third sound will be in the center."))
-        label.set_line_wrap(gtk.TRUE)
-        label.set_size_request(400, -1)
-        label.set_alignment(0.0, 0.5)
+        if not cards:
+            return None, None
+        else:
+            self.mainVBox = gtk.VBox()
+
+            title = gtk.Label(self.windowTitle)
+            title.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse ("white"))
+
+            titleBox = gtk.HBox()
+
+            #Add icon to the top frame
+            self.icon = functions.imageFromFile("multimedia.png")
+
+            if self.icon:
+                titleBox.pack_start(self.icon, gtk.FALSE, gtk.TRUE, 5)
+
+            titleBox.pack_start(title)
+
+            eventBox = gtk.EventBox()
+            eventBox.add(titleBox)
+            eventBox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse ("#cc0000"))
+            self.mainVBox.pack_start(eventBox, gtk.FALSE)
+
+            internalVBox = gtk.VBox(gtk.FALSE, 10)
+            internalVBox.set_border_width(10)
 
 
-        self.myVbox = gtk.VBox()
-        self.myVbox.pack_start(label, gtk.FALSE, gtk.TRUE)
-        self.midHbox = gtk.HBox()
-        self.midHbox.pack_start(self.notebook, gtk.FALSE)
-        self.myVbox.pack_start(self.midHbox, gtk.FALSE)
+            label = gtk.Label(_("Click the "
+                                "\"Play test sound\" button to hear a sample sound.  You should "
+                                "hear a series of three sounds.  The first sound will be in the "
+                                "right channel, the second sound will be in the left channel, "
+                                "and the third sound will be in the center."))
+            label.set_line_wrap(gtk.TRUE)
+            label.set_size_request(400, -1)
+            label.set_alignment(0.0, 0.5)
 
-    def launch(self, doDebug=None):
-        self.doDebug = doDebug
-        if doDebug:
-            print "in mouse launch"
-        self.setupScreen()
-        return FirstbootGuiWindow.launch(self)
+            messageLabel = gtk.Label(self.shortMessage)
+            messageLabel.set_line_wrap(gtk.TRUE)
+            messageLabel.set_size_request(400, -1)
+            messageLabel.set_alignment(0.0, 0.5)
+
+            self.mainVBox.pack_start(internalVBox, gtk.FALSE)
+            internalVBox.pack_start(messageLabel, gtk.FALSE)
+            internalVBox.pack_start(label, gtk.FALSE)
+
+            hbox = gtk.HBox()
+            hbox.pack_start(self.notebook, gtk.FALSE)
+            
+            internalVBox.pack_start(hbox, gtk.FALSE)
+
+            return self.mainVBox, eventBox
 
     def apply(self, *args):
         return 1
-
-
 
 childWindow = SoundWindow
