00d62bea50bca3cc1476eda7aefb693d6cee20d5
bfox <bfox>
2002-09-03 22:24:51 +0000

    create a ditheredImageFromFile function

--- src/functions.py
+++ src/functions.py
@@ -21,6 +21,33 @@ def imageFromFile(filename):
         pix.set_from_pixbuf(p)        
         return pix
 
+# Attempt to load a gtk.Image from a file.
+def ditheredImageFromFile(filename, height = None, width = None):
+    try:
+        path = "pixmaps/" + filename
+        pixbuf = gtk.gdk.pixbuf_new_from_file(path)
+    except:
+        try:
+            path = "/usr/share/firstboot/pixmaps/" + filename
+            pixbuf = gtk.gdk.pixbuf_new_from_file(path)
+        except:
+            pass
+
+    if (height is not None and width is not None
+        and height != pixbuf.get_height()
+        and width != pixbuf.get_width()):
+        pixbuf = pixbuf.scale_simple(height, width,
+                                     gtk.gdk.INTERP_BILINEAR)
+
+    (pixmap, mask) = pixbuf.render_pixmap_and_mask()
+    pixbuf.render_to_drawable(pixmap, gtk.gdk.gc_new(pixmap), 0, 0, 0, 0,
+                              pixbuf.get_width(), pixbuf.get_height(),
+                              gtk.gdk.RGB_DITHER_MAX, 0, 0)
+
+    pix = gtk.Image()
+    pix.set_from_pixmap(pixmap, mask)
+    return pix
+
 def imageFromPath(filename):
     p = None        
     try:
