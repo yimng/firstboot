e40092e1265ae278704c76a17fdad2255ba94cd4
bfox <bfox>
2002-12-02 21:09:05 +0000

    improve message string

--- src/textWindow.py
+++ src/textWindow.py
@@ -13,14 +13,19 @@ translate.textdomain ("firstboot")
 class TextWindow:
     def __call__(self, screen):
         bb = ButtonBar(screen, [_("Yes"), _("No")])
-        t = TextboxReflowed(40, 
-                _("Some text for firstboot here.  Do you wish to run firstboot now?"))
+        t = TextboxReflowed(50, 
+                _("The Red Hat Setup Agent can assist you in configuring your machine "
+                  "by guiding you through a series of steps.  Setup Agent requires the X Window System "
+                  "to run.  \n\n"
+                  "If you wish to run the Setup Agent, "
+                  "please choose \"Yes\".  If you choose \"No\", the system will continue booting and "
+                  "you will never see this message again.  Do you wish to run the Setup Agent now?"))
 
         g = GridFormHelp(screen, _("Red Hat Setup Agent"), "kbdtype", 1, 4)
         g.add(t, 0, 0)
         g.add(bb, 0, 3, growx = 1)
 
-        g.setTimer(20000)
+        g.setTimer(25000)
         
         rc = g.runOnce()
 
