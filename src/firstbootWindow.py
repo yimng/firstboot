853108d0b2e9f293176c95976448118ecf4c0421
bfox <bfox>
2002-12-02 19:03:29 +0000

    move part of writeSysconfigFile to functions.py

--- src/firstbootWindow.py
+++ src/firstbootWindow.py
@@ -304,18 +304,12 @@ class firstbootWindow:
         except:
             pass
 
-        self.writeSysconfigFile()
+        #Call exitFirstboot to do some cleanup before exiting
+        self.exitFirstboot()
 
-    def writeSysconfigFile(self, *args):
-        #Write the /etc/sysconfig/firstboot file to tell firstboot not to run again
-        if (not self.doDebug):
-            fd = open("/etc/sysconfig/firstboot", "w")
-            fd.write("RUN_FIRSTBOOT=NO\n")
-            fd.close()
-
-            #Turn off the firstboot init script
-            path = "/sbin/chkconfig --level 35 firstboot off"
-            os.system(path)
+    def exitFirstboot(self, *args):
+        #Write the /etc/sysconfig/firstboot file to make sure firstboot doesn't run again
+        functions.writeSysconfigFile(self.doDebug)
 
         #Exit the GTK loop
         gtk.mainquit()
