33105f187d65040231f6c02457f03883ee490ec7
bfox <bfox>
2003-06-16 19:49:43 +0000

    make apply work

--- src/modules/timezone.py
+++ src/modules/timezone.py
@@ -92,117 +92,13 @@ class TimeWindow(FirstbootGuiWindow):
         dialog.hide ()
 
     def apply(self, *args):
-        self.failedFlag = None
-        
-##         if self.doDebug:
-##             print "applying date changes not available in debug mode"
-##         else:
-##             sysDate = self.datePage.getDate()
-##             sysTime = self.datePage.getTime()
-##             ntpEnabled = self.datePage.getNtpEnabled()
-
-##             if ntpEnabled == gtk.FALSE:
-##                 #We're not using ntp, so stop the service
-##                 self.dateBackend.stopNtpService()
-##                 #set the time on the system according to what the user set it to
-##                 self.dateBackend.writeDateConfig(sysDate, sysTime)
-##                 self.dateBackend.syncHardwareClock()
-##                 self.dateBackend.chkconfigOff()
-
-##             elif ntpEnabled == gtk.TRUE:
-##                 sysTimeServer = self.datePage.getTimeServer()
-
-##                 if sysTimeServer == "":
-##                     #They want ntp but have not set a server
-##                     text = (_("Please enter a time server."))
-##                     dlg = gtk.MessageDialog(None, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, text)
-##                     dlg.set_title(_("Error"))
-##                     dlg.set_default_size(100, 100)
-##                     dlg.set_position (gtk.WIN_POS_CENTER_ON_PARENT)
-##                     dlg.set_border_width(2)
-##                     dlg.set_modal(gtk.TRUE)
-##                     rc = dlg.run()
-##                     dlg.destroy()
-##                     self.datePage.ntpCombo.entry.grab_focus()
-##                     return
-
-##                 ntpServerList = self.datePage.getNtpServerList()
-##                 self.dateBackend.writeNtpConfig(sysTimeServer, ntpServerList)
-
-##                 def child_handler (signum, stack_frame):
-##                     if not pid:
-##                         return
-                    
-##                     realpid, waitstat = os.waitpid(pid, os.WNOHANG)
-                        
-##                     if realpid != pid:
-##                         return
-##                     gtk.mainquit()
-##                     result = os.read (read,100)
-##                     os.close (read)
-##                     signal.signal (signal.SIGCHLD, signal.SIG_DFL)
-                    
-##                     if int(result) > 0:
-##                         text = (_("A connection with %s could not be established.  "
-##                                                "Either %s is not available or the firewall settings "
-##                                                "on your computer are blocking NTP connections." %
-##                                                (sysTimeServer, sysTimeServer)))
-
-##                         dlg = gtk.MessageDialog(None, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, text)
-
-##                         dlg.set_title(_("Error"))
-##                         dlg.set_default_size(100, 100)
-##                         dlg.set_position (gtk.WIN_POS_CENTER_ON_PARENT)
-##                         dlg.set_border_width(2)
-##                         dlg.set_modal(gtk.TRUE)
-##                         rc = dlg.run()
-##                         dlg.destroy()
-##                         self.failedFlag = 1
-##                         return
-
-##                     self.dateBackend.syncHardwareClock()
-
-##                 signal.signal (signal.SIGCHLD, child_handler)
-##                 (read, write) = os.pipe ()
-##                 pid = os.fork ()
-
-##                 if pid == 0:
-##                     signal.signal (signal.SIGCHLD, signal.SIG_DFL)
-##                     # do something slow
-##                     os.close (read)
-##                     time.sleep (2)
-##                     retval = self.dateBackend.startNtpService(None)
-##                     retval = str(retval)
-##                     os.write (write, retval)
-##                     os._exit (0)
-
-##                 os.close (write)
-
-##                 dlg = gtk.Dialog('', None, 0, (gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL))
-##                 dlg.set_border_width(10)
-##                 label = gtk.Label(_("Contacting NTP server.  Please wait..."))
-##                 dlg.vbox.set_spacing(5)
-##                 dlg.vbox.add(label)
-##                 dlg.set_position (gtk.WIN_POS_CENTER)
-##                 dlg.set_modal(gtk.TRUE)
-##                 dlg.connect ('response', self.response_cb, pid)
-##                 dlg.show_all()
-
-##                 #work around http://bugzilla.gnome.org/show_bug.cgi?id=72333 )-;
-##                 id = gtk.timeout_add(20, lambda:1)
-##                 gtk.mainloop()
-##                 dlg.destroy()
-##                 gtk.idle_remove (id)          
-
-##                 self.dateBackend.chkconfigOn()
-
-        if self.failedFlag:
-            return None
+        if self.doDebug:
+            print "applying timezone changes not available in debug mode"
         else:
-            return 0
-
-    def grabFocus(self):
-        pass
-#        self.datePage.updateSpinButtons()
+            #Get the timezone info from the timezone page
+            timezone, utc, arc = self.timezonePage.getTimezoneInfo()
+            self.timezoneBackend.writeConfig(timezone, utc, arc)
+           
+        return 0
 
 childWindow = TimeWindow
