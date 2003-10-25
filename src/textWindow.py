d398627b4c9e28290c82fc2ff87fdecf38dcfad1
bfox <bfox>
2003-10-25 00:25:41 +0000

    rework textWindow for TUI mode

--- src/textWindow.py
+++ src/textWindow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from snack import *
+import os
 
 ##
 ## I18N
@@ -12,27 +13,45 @@ translate.textdomain ("firstboot")
 
 class TextWindow:
     def __call__(self, screen):
-        bb = ButtonBar(screen, [_("Yes"), _("No")])
-        t = TextboxReflowed(50, 
-                _("The Red Hat Setup Agent can assist you in configuring your machine "
-                  "by guiding you through a series of steps.  Setup Agent requires the X Window System "
-                  "to run.  \n\n"
-                  "If you wish to run the Setup Agent, "
-                  "please choose \"Yes\".  If you choose \"No\", the system will continue booting and "
-                  "you will never see this message again.  Do you wish to run the Setup Agent now?"))
+        toolList = [(_("Authentication"), ("/usr/sbin/authconfig")),
+                    (_("Firewall configuration "), ("/usr/bin/redhat-config-securitylevel-tui")),
+                    (_("Keyboard configuration "), ("/usr/bin/redhat-config-keyboard --text")),
+                    (_("Mouse configuration "), ("/usr/bin/redhat-config-mouse --text")),
+                    (_("Network configuration "), ("/usr/sbin/netconfig")),
+                    (_("Printer configuration "), ("/usr/sbin/printconf-tui")),
+                    (_("System services "), ("/usr/sbin/ntsysv")),
+                    (_("Sound card configuration "), ("/usr/sbin/sndconfig")),
+                    (_("Timezone configuration "), ("/usr/sbin/timeconfig"))]
 
-        g = GridFormHelp(screen, _("Red Hat Setup Agent"), "kbdtype", 1, 4)
-        g.add(t, 0, 0)
-        g.add(bb, 0, 3, growx = 1)
+        bb = ButtonBar(screen, [_("Run Tool"), _("Exit")])
 
-        g.setTimer(25000)
-        
-        rc = g.runOnce()
+        textbox = TextboxReflowed(50, _("Select the item that you wish to modify "))
+        self.listbox = Listbox(8, scroll=1, returnExit=0)
 
-        button = bb.buttonPressed(rc)
+        for name, tool in toolList:
+            self.listbox.append(name, tool)
 
-        if button == "yes":
-            return 0
-        else:
+        gridForm = GridFormHelp(screen, _("Setup Agent"), "", 1, 4)
+        gridForm.add(textbox, 0, 0)
+        gridForm.add(self.listbox, 0, 1, padding = (0, 1, 0, 1))
+        gridForm.add(bb, 0, 3, growx = 1)
+
+        #Set a timeout limit so that firstboot won't hang around forever on headless machines
+        gridForm.setTimer(50000)
+
+        answer = gridForm.runOnce()
+
+        if answer == "TIMER":
+            #We've reached the timeout limit.  Let's return -1
             return -1
 
+        button = bb.buttonPressed(answer)
+
+        if button == "exit":
+            return -1
+        else:
+            #They've selected to run a config tool.  Lauch it now
+            choice = self.listbox.current()
+            os.system(choice)
+            return 0
+        
