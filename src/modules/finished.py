f562c51b8e6ffaddfbc1c1d1960a43297d8e16bf
alikins <alikins>
2004-07-14 18:00:32 +0000

    look for message left by other modules to display in the finish screen, put them in order, and show them.
    Used to allow the finish screen to include info
    custom tailored to the user

--- src/modules/finished.py
+++ src/modules/finished.py
@@ -2,8 +2,10 @@ from gtk import *
 import string
 import gtk
 import gobject
+import os
 import sys
 import functions
+import glob
 
 ##
 ## I18N
@@ -30,19 +32,39 @@ class childWindow:
         internalVBox = gtk.VBox()
         internalVBox.set_border_width(10)
 
-        label = gtk.Label(_("Your system is now set up and ready to use.  Please "
-                            "click the \"Next\" button in the lower right corner to continue."))
+        self.txt = _("Your system is now set up and ready to use.  Please "
+                            "click the \"Next\" button in the lower right corner to continue.")
 
-        label.set_line_wrap(gtk.TRUE)
-        label.set_alignment(0.0, 0.5)
-        label.set_size_request(500, -1)
-        internalVBox.pack_start(label, FALSE, TRUE)
+        self.label = gtk.Label(self.txt)
+            
+        self.label.set_line_wrap(gtk.TRUE)
+        self.label.set_alignment(0.0, 0.5)
+        self.label.set_size_request(500, -1)
+        internalVBox.pack_start(self.label, FALSE, TRUE)
         
         self.vbox.pack_start(internalVBox, gtk.FALSE, 5)
         pix = functions.ditheredImageFromFile("splash-small.png")
         self.vbox.pack_start(pix, gtk.TRUE, gtk.TRUE, 5)
 
         return self.vbox, title_pix, self.moduleName
+
+    def updatePage(self):
+        if os.access("/var/tmp/firstboot", os.R_OK):
+            files = glob.glob("/var/tmp/firstboot/*_firstboot_finish_message")
+            # the file names are in the format 37_firstboot_finish_message with the
+            # number indicating a sorting priority, so sort them...
+            files.sort()
+            for file in files:
+                fd = open(file, "r")
+                new_txt = fd.read()
+                self.txt = self.txt + "\n\n"
+                self.txt = self.txt + new_txt
+
+        # dont need these files anymore
+        for file in files:
+            os.unlink(file)
+            
+        self.label.set_text(self.txt)
 
     def apply(self, notebook):
         return 0
