72c058bd8757279cfe6709010eeae0dff012345c
Chris Lumens <clumens@redhat.com>
2005-10-17 15:38:35 +0000

    Relabel button to "next" if they get to the last page and go back.

--- src/firstbootWindow.py
+++ src/firstbootWindow.py
@@ -345,6 +345,8 @@ class firstbootWindow:
 
         self.nextButton.disconnect(self.nextHandler)
         self.nextHandler = self.nextButton.connect('clicked', self.nextClicked)
+        self.nextLabel.set_text(_("_Next"))
+        self.nextLabel.set_property("use_underline", True)
         self.win.disconnect(self.winHandler)
         self.winHandler = self.win.connect ("key-release-event", self.keyRelease)
 
