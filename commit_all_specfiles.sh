2e59241ff9ccf298ac793bf59efcc213993f0b98
Chris Lumens <clumens@redhat.com>
2006-01-19 19:05:32 +0000

    Remove lots of unused stuff.

--- commit_all_specfiles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ..
-cvs commit -m 'updated changelog and bump rev number' firstboot/firstboot.spec redhat-config-keyboard/redhat-config-keyboard.spec redhat-config-language/redhat-config-language.spec redhat-config-mouse/redhat-config-mouse.spec redhat-config-rootpassword/redhat-config-rootpassword.spec redhat-config-securitylevel/redhat-config-securitylevel.spec redhat-config-soundcard/redhat-config-soundcard.spec redhat-config-nfs/redhat-config-nfs.spec redhat-config-samba/redhat-config-samba.spec
