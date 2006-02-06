bd5673ec1f1b44ee43f16f109a97aca66d77627e
Chris Lumens <clumens@redhat.com>
2006-02-06 15:40:13 +0000

    Make sure a program exists before adding it to the list of services to run.

--- src/textWindow.py
+++ src/textWindow.py
@@ -31,21 +31,23 @@ translate.textdomain ("firstboot")
 
 class TextWindow:
     def __call__(self, screen):
-        toolList = [(_("Authentication"), ("/usr/sbin/authconfig-tui")),
-                    (_("Firewall configuration"), ("/usr/bin/system-config-securitylevel-tui")),
-                    (_("Keyboard configuration"), ("/usr/bin/system-config-keyboard --text")),
-                    (_("Network configuration"), ("/usr/sbin/netconfig")),
-                    (_("Printer configuration"), ("/usr/sbin/printconf-tui")),
-                    (_("System services"), ("/usr/sbin/ntsysv")),
-                    (_("Timezone configuration"), ("/usr/sbin/timeconfig"))]
+        toolList = [(_("Authentication"), ("/usr/sbin/authconfig-tui"), ("")),
+                    (_("Firewall configuration"), ("/usr/bin/system-config-securitylevel-tui"), ("")),
+                    (_("Keyboard configuration"), ("/usr/bin/system-config-keyboard"), ("--text")),
+                    (_("Network configuration"), ("/usr/sbin/netconfig"), ("")),
+                    (_("Printer configuration"), ("/usr/sbin/printconf-tui"), ("")),
+                    (_("System services"), ("/usr/sbin/ntsysv"), ("")),
+                    (_("Timezone configuration"), ("/usr/sbin/timeconfig"), (""))]
 
         bb = ButtonBar(screen, [_("Run Tool"), TEXT_EXIT_BUTTON])
 
         textbox = TextboxReflowed(50, _("Select the item that you wish to modify "))
         self.listbox = Listbox(8, scroll=1, returnExit=0)
 
-        for name, tool in toolList:
-            self.listbox.append(name, tool)
+        for name, tool, args in toolList:
+            # Make sure we can access the program before adding it.
+            if os.access(tool, os.X_OK):
+                self.listbox.append(name, string.join([tool, args]))
 
         gridForm = GridFormHelp(screen, _("Setup Agent"), "", 1, 4)
         gridForm.add(textbox, 0, 0)
