1e8b706ce6a000e34969e62cbe88477ce3741d06
Chris Lumens <clumens@exeter.boston.redhat.com>
2007-11-02 14:06:14 -0400

    Add the start_process function, which is needed for additional_cds.

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,5 @@
 import gtk
+import os
 
 def loadPixbuf(fn):
     return gtk.gdk.pixbuf_new_from_file(fn)
@@ -9,3 +10,17 @@ def loadToImage(fn):
     pixWidget = gtk.Image()
     pixWidget.set_from_pixbuf(pix)
     return pixWidget
+
+def start_process(path, args = None):
+    if args == None:
+        args = [path]
+    else:
+        args = [path, args]
+
+    child = os.fork()
+
+    if not child:
+        os.execvp(path, args)
+        os._exit(1)
+
+    return child
