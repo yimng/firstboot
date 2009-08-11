9354cbb1c3d994734b403664f01c5e781d4cf8e9
Chris Lumens <clumens@redhat.com>
2009-08-11 14:21:59 -0400

    New version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 from glob import *
 
-setup(name='firstboot', version='1.107',
+setup(name='firstboot', version='1.108',
       description='Post-installation configuration utility',
       author='Chris Lumens', author_email='clumens@redhat.com',
       url='http://fedoraproject.org/wiki/FirstBoot',
