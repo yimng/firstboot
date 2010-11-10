3e9bb94d0770ffbced45a032cbc6c9b4a5fa6619
Martin Gracik <mgracik@redhat.com>
2010-11-10 18:03:11 +0100

    Do not show tabs in date and time module
    system-config-date no longer has separate notebook tabs
    for datetime and ntp, so we only want to show one tab,
    the datetime tab.

--- modules/date.py
+++ modules/date.py
@@ -64,7 +64,7 @@ class moduleClass(Module):
 	label.set_size_request(500, -1)
 	self.vbox.pack_start(label, False, True, padding=20)
 
-	self.scd = scdMainWindow(firstboot=True, showPages=["datetime", "ntp"])
+	self.scd = scdMainWindow(firstboot=True, showPages=["datetime"])
         self.vbox.pack_start(self.scd.firstboot_widget(), False, False)
 
     def initializeUI(self):
